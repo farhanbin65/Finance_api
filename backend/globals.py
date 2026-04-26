da57e9c8d56ac47e7f549fed35f3b26191ba3d2f
farhanbin65 <your.email@example.com>
2026-04-26 13:10:15 +0100

    globals.py updated

--- backend/globals.py
+++ backend/globals.py
@@ -1,13 +1,11 @@
 from pymongo import MongoClient
 import os
-import ssl
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'mysecret')
 MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/')
 
 client = MongoClient(
     MONGO_URI,
-    ssl=True,
-    ssl_cert_reqs=ssl.CERT_NONE
+    tlsAllowInvalidCertificates=True
 )
 db = client.finance_DB

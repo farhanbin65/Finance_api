e1e206435af408f83899455c5bf375c402629923
farhanbin65 <your.email@example.com>
2026-04-26 12:48:11 +0100

    globas updadted

--- backend/globals.py
+++ backend/globals.py
@@ -4,5 +4,9 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY', 'mysecret')
 MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/')
 
-client = MongoClient(MONGO_URI)
+client = MongoClient(
+    MONGO_URI,
+    tls=True,
+    tlsAllowInvalidCertificates=True
+)
 db = client.finance_DB
